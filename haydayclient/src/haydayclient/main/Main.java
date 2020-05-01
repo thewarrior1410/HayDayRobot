301421bc4a7735419385d7ff57f1838319ac56de
thewarrior1410 <64496883+thewarrior1410@users.noreply.github.com>
2020-05-01 16:39:25 +0200

    Find and CloseTo Methode

--- haydayclient/src/haydayclient/main/Main.java
+++ haydayclient/src/haydayclient/main/Main.java
@@ -5,6 +5,7 @@ import java.awt.AWTException;
 import java.awt.Color;
 import java.awt.Rectangle;
 import java.awt.Robot;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
@@ -20,15 +21,13 @@ public class Main {
 			return;
 		}
 		
-		BufferedImage bImg = r.createScreenCapture(new Rectangle(0,0,500,500));
+		r.delay(5000);
 		
-		ArrayList<Coordinates> s = findColoredPixels(bImg, new Color(47, 47, 47));
-		//System.out.println(s.toString());
+		BufferedImage bImg = r.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
 		
-		for(Coordinates c : s) {
-			System.out.println("x: "+c.getX()+", y: "+c.getY());
-		}
-		
+		ArrayList<Coordinates> s = findWheat(bImg);
+		System.out.println("Gefundene Wheat Coords");
+		printCoordArray(s);
 		
 	}
 	
@@ -37,16 +36,18 @@ public class Main {
 	
 		ArrayList<Coordinates> a = new ArrayList<Coordinates>();
 		int height = bImg.getHeight();
+		System.out.println("height"+height);
 		int width = bImg.getWidth();
+		System.out.println("width"+width);
 		
-		for (int x = 0; x < height; x++) {
+		for (int x = 0; x < width; x++) {
 			
-			for (int y = 0; y < width; y++) {
+			for (int y = 0; y < height; y++) {
 				
 				Color bcolor = new Color(bImg.getRGB(x, y));
-				System.out.println(bcolor.toString());
+				//System.out.println(bcolor.toString());
 				
-				if (bcolor.equals(c)) {
+				if (closeTo(bcolor, c)) {
 					a.add(new Coordinates(x, y));
 				}
 				
@@ -61,4 +62,71 @@ public class Main {
 		
 	}
 	
+	public static ArrayList<Coordinates> findWheat(BufferedImage bImg) {
+		
+		Color c1 = new Color(254, 214, 21);
+		Color c2 = new Color(246, 216, 15);
+		Color c3 = new Color(244, 215, 12);
+		
+		
+		ArrayList<Coordinates> cFound1 = findColoredPixels(bImg, c1);
+		ArrayList<Coordinates> cFound2 = new ArrayList<Coordinates>();
+		ArrayList<Coordinates> cFound3 = new ArrayList<Coordinates>();
+		
+		System.out.println("found1");
+		printCoordArray(cFound1);
+		
+		//coord.getX()+2 darf nicht größer als Bildschirm sein !!!!!!
+		for (Coordinates coords : cFound1) {
+			Color bcolor = new Color(bImg.getRGB(coords.getX()+1, coords.getY()));
+			if (closeTo(bcolor, c2)) {
+				cFound2.add(coords);
+			}
+		}
+		
+		System.out.println("found2");
+		printCoordArray(cFound2);
+		
+		for (Coordinates coords : cFound2) {
+			Color bcolor = new Color(bImg.getRGB(coords.getX()+2, coords.getY()));
+			if (closeTo(bcolor, c3)) {
+				cFound3.add(coords);
+			}
+		}
+		
+		System.out.println("found3");
+		printCoordArray(cFound3);
+		
+		return cFound3;
+	}
+	
+	public static void printCoordArray(ArrayList<Coordinates> a) {
+		for(Coordinates c : a) {
+			System.out.println("x: "+c.getX()+", y: "+c.getY());
+		}
+	}
+
+	public static boolean closeTo(Color c1, Color c2) {
+		
+		float[] c1RGB = new float[3];
+		float[] c2RGB = new float[3];
+		
+		c1RGB[0] = c1.getRed();
+		c2RGB[0] = c2.getRed();
+		c1RGB[1] = c1.getGreen();
+		c2RGB[1] = c2.getGreen();
+		c1RGB[2] = c1.getBlue();
+		c2RGB[2] = c2.getBlue();
+		
+		for (int i = 0; i < c1RGB.length; i++) {
+			
+			if (c1RGB[i]-10 < c2RGB[i] && c2RGB[i] < c1RGB[i]+10) {
+				continue;
+			}
+			else {
+				return false;
+			}
+		}
+		return true;
+	}
 }
